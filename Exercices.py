0aae2b0f0c0a2e724eb432cf76192da1c92b8a5e
harrissonl <harrissonl@educ.college-montreal.qc.ca>
2025-11-26 14:22:55 -0500

    Commit initial

---
+++ Exercices.py
@@ -0,0 +1,33 @@
+"""
+Louka Harrisson
+406
+Exercices python
+"""
+
+"""
+class Epee:
+    def __init__(self, nom, force, dura):
+        self.force = force
+        self.nom = nom
+        self.dura = dura
+
+    def perdre_dura(self):
+        self.dura -= 3
+
+
+
+e1 = Epee("diamant", 5, 6)
+e1.perdre_dura()
+
+e2 = Epee("fer", 3, 3)
+e2 = perdre.dura()
+"""
+
+class Stringfoo:
+    def __init__(self):
+        self.message = ""
+
+    def set_string(self, msg):
+        self
+
+s1 = Stringfoo()
